84e438b23bc18f4ee7504bbb3f63f971117b33d3
cavallucci <laura.cllucci@gmail.com>
2022-09-28 11:25:25 +0200

    creating database

--- srcs/requirements/mariadb/tools/create_database.sh
+++ srcs/requirements/mariadb/tools/create_database.sh
@@ -1,1 +1,14 @@
-mysql_install_db
+#https://mariadb.com/kb/en/create-database/
+
+
+#Use ping to test if mariadb is correctly in service, while sleeping.
+#service mysql start
+#systemctl status mysql
+#while ! mysqladmin -p -u root ping; do
+#    sleep 2
+#done
+#Create database
+sudo mysql << EOF
+CREATE DATABASE test;
+EOF
+#mysql --user root --execute "CREATE DATABASE TEST;"
