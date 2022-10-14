4d2ffebbc32710b535ebd34af53aea6c98ab9dbc
cavallucci <laura.cllucci@gmail.com>
2022-10-14 18:09:58 +0200

    nginx fichier conf

--- test/restart.sh
+++
@@ -1,5 +0,0 @@
-sudo docker container kill 1th-try
-sudo docker container rm -f 1th-try
-sudo docker image rm simple-test
-sudo docker build -t simple-test .
-sudo docker container run --name=1th-try -d -p 9300:80 simple-test
